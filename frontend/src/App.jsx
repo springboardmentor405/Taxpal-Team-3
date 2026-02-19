22211f669fce770687e6890169f0fd8c8346b875
PARAMASIVAM52 <v.paramasivam1234@gmail.com>
2026-02-19 08:24:19 +0530

    data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import LeftSide from './components/Leftside';
+import Login from './pages/Login';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,7 +30,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
-      <LeftSide/>
+      {/* <LeftSide/> */}
+      <Login/>
     </>
   )
 }
